fd89b98fd1dd486077a0dd9da2330ee805b8f3ea
Vy Nguyen <vy.nguyenduongthuy@gmail.com>
2023-12-27 20:38:26 +0700

    update file

--- app.js
+++ app.js
@@ -18,13 +18,7 @@ app.use(logger("dev"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    methods: ["POST", "PUT", "GET", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", indexRouter);
